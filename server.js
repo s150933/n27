050cd97e833036f52a82ef704b42831b0ec335f8
s150933 <s150933@beruskolleg-borken.de>
2019-04-09 10:58:28 +0200

    ...

--- server.js
+++ server.js
@@ -1,103 +1,98 @@
-// Das ist ein einzeiliger Kommentar.
-// Das ist ein einzeiliger Kommentar.
-
-const bodyParser = require('body-parser')
-
 const express = require('express')
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
 const app = express()
-
- app.set('view engine','ejs')
-
- app.use(bodyParser.urlencoded({extendet : true}))
-
- app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
- const server = app.listen(process.env.PORT  || 3001,()=> {
-     console.log(' Der Server ist erfolgreich gestartet auf Port %s',server.address().port)
- })
 
- app.get('/',(req, res, next) => {
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server lauscht auf Port %s', server.address().port)    
+})
 
-   let idKunde = req.cookies['istAngemeldetAls']
+app.get('/',(req, res, next) => {   
 
+    let idKunde = req.cookies['istAngemeldetAls']
+    
     if(idKunde){
-     
-          console.log("Kunde ist angemeldet als " + idKunde)
-          res.render('index.ejs',)
-        }else{
-          res.render('login.ejs' ,{
-    
-          })
-        }
-     
-     
-    
- })
- app.get('/login',(req, res, next) => {
-    res.render('login.ejs', {
-        
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
+
+app.get('/login',(req, res, next) => {         
+    res.cookie('istAngemeldetAls', '')       
+    res.render('login.ejs', {                    
     })
 })
-app.post('/',(req, res, next) => {
 
-     const idKunde = req.body.idKunde
-     const kennwort = req.body.kennwort
+app.get('/login',(req, res, next) => {   
+    res.render('login.ejs', {                    
+    })
+})
 
-      if(idKunde ==="4711"&& kennwort=== "123" ){
-          console.log("Der Cookie wird gesetzt")
-          res.cookie('istAngemeldetAls',idKunde)
-          res.render('index.ejs', {
+app.post('/',(req, res, next) => {   
 
-          })
-      }else{
+    const idKunde = req.body.idKunde
+    const kennwort = req.body.kennwort
+
+    if(idKunde ==="4711" && kennwort ==="123"){
+        console.log("Der Cookie wird gesetzt")
+        res.cookie('istAngemeldetAls','idKunde')
+        res.render('index.ejs', {                    
+        })
+    }else{
         console.log("Der Cookie wird gelöscht")
         res.cookie('istAngemeldetAls','')
-        res.render('login.ejs', {
-      })  
+        res.render('login.ejs', {                    
+        })
     }
 })
 
 app.get('/impressum',(req, res, next) => {   
 
-  let idKunde = req.cookies['istAngemeldetAls']
-  
-  if(idKunde){
-      console.log("Kunde ist angemeldet als " + idKunde)
-      res.render('impressum.ejs', {                              
-      })
-  }else{
-      res.render('login.ejs', {                    
-      })    
-  }
-}) 
-app.post('/kontoAnlegen',(req, res, next) => {   
-
-  let idKunde = req.cookies['istAngemeldetAls']
-  
-  if(idKunde){
-      console.log("Kunde ist angemeldet als " + idKunde)
-      res.render('kontoAnlegen.ejs', {                              
-      })
-  }else{
-      res.render('login.ejs', {                    
-      })    
-  }
-}) 
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('impressum.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
 
 app.get('/kontoAnlegen',(req, res, next) => {   
 
-  let idKunde = req.cookies['istAngemeldetAls']
-  
-  if(idKunde){
-      console.log("Kunde ist angemeldet als " + idKunde)
-      res.render('kontoAnlegen.ejs', {                              
-      })
-  }else{
-      res.render('login.ejs', {                    
-      })    
-  }
-}) 
- 
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('kontoAnlegen.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
+
+app.post('/kontoAnlegen',(req, res, next) => {   
+
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('kontoAnlegen.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
 
